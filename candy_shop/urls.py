0477a20499d0fea9dc73eaa10bc7b480b38af385
Constantine <m04116@hotmail.com>
2017-01-05 21:29:38 +0200

    Stylesheet in style.css

--- candy_shop/urls.py
+++ candy_shop/urls.py
@@ -16,6 +16,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 import main.views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     url(r'^$', main.views.home),
     url(r'^item/(?P<alias>[^/]+)', main.views.item),
     url(r'^(?P<alias>[^/]+)', main.views.get_category),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
